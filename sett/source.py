02cc730c063f1d862a21e0df313efba739a2eec3
Grégoire ROCHER <gr@enix.org>
2015-10-13 11:06:18 +0200

    Add a command to create a package from a file

--- sett/source.py
+++ sett/source.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from paver.easy import task, path, needs
+from paver.easy import task, path, needs, consume_nargs
 
 
 @task
@@ -10,3 +10,14 @@ def clean_pyc(options):
     for dir in options.setup['packages']:
         for file in path(dir).walkfiles('*.pyc', errors='ignore'):
             path(file).unlink()
+
+
+@task
+@consume_nargs(1)
+def packagize(args):
+    filename, = args
+    filename = path(filename)
+
+    module_root, py = filename.splitext()
+    module_root.mkdir()
+    filename.rename(module_root.joinpath('__init__.py'))
