ae3d75ae751cc2dfd1210298ae6c8865a9c5168d
Grégoire ROCHER <gr@enix.org>
2016-02-19 18:22:08 +0100

    Add a Dispatcher

---
+++ sett/utils/dispatch.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+import heapq
+import types
+import itertools
+import functools
+import collections
+
+from paver.deps.six import with_metaclass
+
+counter = itertools.count()
+
+
+class MetaDispatcher(type):
+    def __new__(self, name, bases, attrs):
+        callbacks = self._collect(attrs)
+        for command_name, callback_list in callbacks.items():
+            callback_list = sorted(callback_list)
+            super_fn = attrs.get(command_name)
+            assert super_fn is None or callable(super_fn)
+
+            if super_fn:
+                heapq.heappush(callback_list, (0, 0, attrs[command_name]))
+
+            fn = self.fn_factory(sorted(callback_list))
+            if super_fn:
+                fn = functools.wraps(super_fn)(fn)
+            else:
+                fn.__name__ = command_name
+                fn.__doc__ = ', then '.join(f[2].__name__ for f in callback_list)
+
+            attrs[command_name] = fn
+        return type.__new__(self, name, bases, attrs)
+
+    @classmethod
+    def _collect(cls, attrs):
+        callbacks = collections.defaultdict(list)
+        for attr in attrs.values():
+            if isinstance(attr, types.FunctionType):
+                events = attr.__dict__.pop('callbacks', [])
+                for command, priority in events:
+                    heapq.heappush(callbacks[command], (priority, -next(counter), attr))
+        return callbacks
+
+    @classmethod
+    def fn_factory(self, args):
+        def fn(self):
+            for x, y, fn in args:
+                fn(self)
+        return fn
+
+    def list(self):
+        return [fn
+                for name, fn in self.__dict__.items()
+                if not name.startswith('_') and callable(fn) and name != 'default'
+                ]
+
+    def commands(self):
+        return {fn.__name__: fn.__doc__ or fn.__name__ for fn in self.list()}
+
+
+class Dispatcher(with_metaclass(MetaDispatcher)):
+
+    @staticmethod
+    def on(command, priority=1):
+        def decorator(fn):
+            assert priority != 0
+            fn.__dict__.setdefault('callbacks', []).append((command, priority))
+            return fn
+        return decorator
+
+    @classmethod
+    def usage(cls):
+        commands = cls.commands()
+        return '{name} [{command_names}]\n{command_doc}'.format(
+            name=cls.__name__,
+            command_names=' | '.join(commands.keys()),
+            command_doc='\n'.join('\t- {}: {}'.format(k, v)
+                                  for k, v in commands.items()),
+        )
+
+    def help(self):
+        sys.stdout.write(self.usage())
+        sys.stdout.write('\n')
+
+    def default(self, name):
+        raise NotImplementedError('Command `{}` does not exist\n Usage: {}'.format(
+            name, self.usage(),
+        ))
+
+    def __call__(self, command):
+        assert not command.startswith('_'), 'Cannot call private methods'
+        cmd = getattr(self, command, None) or functools.partial(self.default, command)
+        cmd()
---
+++ tests/test_utils/test_dispatch.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+
+import unittest
+try:
+    import unittest.mock as mock
+except ImportError:
+    import mock
+
+from sett.utils.dispatch import Dispatcher
+
+
+class TestDispatcher(unittest.TestCase):
+    def test_dispatcher_help(self):
+        class MyDispatcher(Dispatcher):
+            def help(self):
+                'help and stuff'
+                pass
+
+        d = MyDispatcher()
+        self.assertEqual(d.usage(), 'MyDispatcher [help]\n\t- help: help and stuff')
+
+    def test_call(self):
+        m = mock.Mock()
+
+        class MyDispatcher(Dispatcher):
+            def foobar(self):
+                m.foobar()
+
+        d = MyDispatcher()
+        d('foobar')
+        m.foobar.assert_called_once_with()
+
+    def test_call_default(self):
+        m = mock.Mock()
+
+        class MyDispatcher(Dispatcher):
+            def default(self, name):
+                m.default(name)
+
+        d = MyDispatcher()
+        d('foobar')
+        m.default.assert_called_once_with('foobar')
+
+    def test_call_default_raise(self):
+        class MyDispatcher(Dispatcher):
+            pass
+
+        d = MyDispatcher()
+
+        with self.assertRaises(NotImplementedError):
+            d('foobar')
+
+    def test_on(self):
+        m = mock.Mock()
+
+        class MyDispatcher(Dispatcher):
+            @Dispatcher.on('foo')
+            def bar(self):
+                m.bar()
+
+            @Dispatcher.on('foo')
+            def baz(self):
+                m.baz()
+
+        d = MyDispatcher()
+
+        self.assertEqual(d.foo.__doc__, 'bar, then baz')
+
+        d.foo()
+        m.assert_has_calls([
+            mock.call.bar(),
+            mock.call.baz(),
+        ])
+
+    def test_on_super(self):
+        m = mock.Mock()
+
+        class MyDispatcher(Dispatcher):
+            @Dispatcher.on('foo', -1)
+            def before(self):
+                m.before()
+
+            @Dispatcher.on('foo')
+            def after(self):
+                m.after()
+
+            def foo(self):
+                'fighter'
+                m.foo()
+
+        d = MyDispatcher()
+
+        self.assertEqual(d.foo.__doc__, 'fighter')
+
+        d.foo()
+        m.assert_has_calls([
+            mock.call.before(),
+            mock.call.foo(),
+            mock.call.after(),
+        ])
