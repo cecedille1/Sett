9f95bb0dd330643345ded9dfd2433f197d326323
Grégoire ROCHER <gr@enix.org>
2016-03-01 12:21:30 +0100

    Do not pollute global variables in tests

--- tests/test_quality.py
+++ tests/test_quality.py
@@ -1,38 +1,49 @@
 # -*- coding: utf-8 -*-
 
 
+import unittest
 try:
     from unittest import mock
 except ImportError:
     import mock
 
-from paver.easy import environment, Bunch
+from paver.tasks import Environment, task
+from paver.options import Bunch
 from sett.quality import quality
 
 
-def test_quality_warning_code():
-    environment.options = Bunch({'setup': Bunch({'packages': []})})
-    with mock.patch('sett.quality.sh') as sh:
-        sh.return_value = '''
+environment = Environment(__import__('tests.test_quality'))
+
+
+@task
+def setup_options():
+    pass
+
+
+@mock.patch('paver.tasks.environment', environment)
+class Test_quality(unittest.TestCase):
+    def test_quality_warning_code(self):
+        environment.options = Bunch({'setup': Bunch({'packages': []})})
+        with mock.patch('sett.quality.sh') as sh:
+            sh.return_value = '''
 test/test_quality.py:14:9: E121 this and that
 test/test_quality.py:14:9: W292 this and that
 '''
-        try:
-            quality()
-        except SystemExit:
-            raise AssertionError('should not have raised SystemExit')
+            try:
+                quality()
+            except SystemExit:
+                raise AssertionError('should not have raised SystemExit')
 
-
-def test_quality_error_code():
-    environment.options = Bunch({'setup': Bunch({'packages': []})})
-    with mock.patch('sett.quality.sh') as sh:
-        sh.return_value = '''
+    def test_quality_error_code(self):
+        environment.options = Bunch({'setup': Bunch({'packages': []})})
+        with mock.patch('sett.quality.sh') as sh:
+            sh.return_value = '''
 test/test_quality.py:14:9: E121 this and that
 test/test_quality.py:14:9: W504 this and that
 '''
-        try:
-            quality()
-        except SystemExit:
-            pass
-        else:
-            raise AssertionError('should have raised SystemExit')
+            try:
+                quality()
+            except SystemExit:
+                pass
+            else:
+                raise AssertionError('should have raised SystemExit')
